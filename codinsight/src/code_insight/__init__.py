113c29c0a5687807ef4844ce9b10f5b0c36b3918
applyuser160 <syunsuke130121@gamil.com>
2025-08-17 19:44:10 +0900

    CodeAnalysisType追加

--- codinsight/src/code_insight/__init__.py
+++ codinsight/src/code_insight/__init__.py
@@ -1,10 +1,11 @@
 from .code_analysis.struct import Struct, StructAnalysisResult
 from .code_analysis.style import Style, StyleAnalysisResult
-from .core import CodeAnalysis
+from .core import CodeAnalysis, CodeAnalysisType
 from .trend_analysis.trend_analysis import TrendAnalysis
 
 __all__ = [
     "CodeAnalysis",
+    "CodeAnalysisType",
     "Style",
     "StyleAnalysisResult",
     "Struct",
