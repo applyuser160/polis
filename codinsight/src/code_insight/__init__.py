04ac820e5e1f13d2923c48c4fce69d83272e678d
applyuser160 <syunsuke130121@gamil.com>
2025-08-17 11:28:04 +0900

    init

---
+++ codinsight/src/code_insight/__init__.py
@@ -0,0 +1,11 @@
+from .code_analysis.struct import Struct, StructAnalysisResult
+from .code_analysis.style import Style, StyleAnalysisResult
+from .core import CodeAnalysis
+
+__all__ = [
+    "CodeAnalysis",
+    "Style",
+    "StyleAnalysisResult",
+    "Struct",
+    "StructAnalysisResult",
+]
